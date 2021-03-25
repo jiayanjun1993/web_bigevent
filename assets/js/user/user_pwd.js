4604d57874b41ec89af982cd2f2abaa299697e6c
jiayanjun <670844629@qq.com>
2021-03-25 08:16:13 +0800

    完成个人中心功能的开发

---
+++ assets/js/user/user_pwd.js
@@ -0,0 +1,39 @@
+$(function () {
+    var form = layui.form
+
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        samePwd: function (value) {
+            if (value === $('[name=oldPwd]').val()) {
+                return '新旧密码不能相同'
+            }
+        },
+        rePwd: function (value) {
+            if (value !== $('[name = newPwd]').val()) {
+                return '两次密码输入不一致！'
+            }
+        }
+    })
+
+    $('.layui-form').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/updatepwd',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('更新密码失败！')
+                }
+                layui.layer.msg('更新密码成功！')
+
+                // [0] 把jQuery对象转换为DOM对象，通过 reset() 方法    重置表单
+                $('.layui-form')[0].reset()
+            }
+        })
+    })
+})
